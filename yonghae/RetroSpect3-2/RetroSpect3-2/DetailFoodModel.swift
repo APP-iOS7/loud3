ce6b524cf0b5aaec30e6417b4fe9a76e4f5ad033
김용해 <yoho1019@gmail.com>
2025-01-19 00:00:00 +0900

    model의 color를 옵션으로 해서 다른 초기화 합수 생성

--- yonghae/RetroSpect3-2/RetroSpect3-2/DetailFoodModel.swift
+++ yonghae/RetroSpect3-2/RetroSpect3-2/DetailFoodModel.swift
@@ -11,5 +11,19 @@ struct DetailFoodModel: Identifiable, Hashable {
     var id = UUID()
     var title: String
     var imageName: String
-    var color: Color
+    var color: Color?
+    
+    init(id: UUID = UUID(), title: String, imageName: String, color: Color) {
+        self.id = id
+        self.title = title
+        self.imageName = imageName
+        self.color = color
+    }
+    
+    // dumyCategoryImageData 를 위해 배경색은 동일 하므로 Color를 옵셔널로 바꿉니다
+    init(id: UUID = UUID(), title: String, imageName: String) {
+        self.id = id
+        self.title = title
+        self.imageName = imageName
+    }
 }
