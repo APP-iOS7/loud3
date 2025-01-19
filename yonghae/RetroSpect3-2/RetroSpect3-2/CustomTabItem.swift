5969960f7b0f64de36e4aa6f35cfc70e1243a26b
김용해 <yoho1019@gmail.com>
2025-01-19 18:32:00 +0900

    내부 프로퍼티 바인딩 및 초기화 함수 래핑된값 넣기 및 onTapGesture 로직 추가

--- yonghae/RetroSpect3-2/RetroSpect3-2/CustomTabItem.swift
+++ yonghae/RetroSpect3-2/RetroSpect3-2/CustomTabItem.swift
@@ -10,17 +10,24 @@ import SwiftUI
 struct CustomTabItem<Content: View>: View {
     var image: String?
     var tabItemName: String?
+    var isSelection: PageChangeType
+    @Binding var navigationSelection: PageChangeType
     let content: Content?
     
+    
     // content가 있는 생성자
-    init(@ViewBuilder content: @escaping () -> Content) {
+    init(navigationSelection: Binding<PageChangeType> ,isSelection: PageChangeType ,@ViewBuilder content: @escaping () -> Content) {
+        self.isSelection = isSelection
+        self._navigationSelection = navigationSelection
         self.content = content()
     }
     
     // content가 없는 생성자
-    init(image: String, tabItemName: String) where Content == EmptyView {
+    init(image: String, tabItemName: String,isSelection: PageChangeType, navigationSelection: Binding<PageChangeType>) where Content == EmptyView {
         self.image = image
         self.tabItemName = tabItemName
+        self.isSelection = isSelection
+        self._navigationSelection = navigationSelection
         self.content = nil
     }
     
@@ -28,6 +35,15 @@ struct CustomTabItem<Content: View>: View {
 
         if let content = content {
             content
+                .onTapGesture {
+                    // 클로저 함수 content 가 있을 경우 이 두개의 PageChangetype이 다를 때 페이지 이동
+                    withAnimation {
+                        // 이 두개의 PageChangetype이 다를 때 페이지 이동
+                        if navigationSelection != isSelection {
+                            navigationSelection = isSelection
+                        }
+                    }
+                }
         } else {
             VStack(spacing: 5) {
                 Image(image!)
@@ -41,6 +57,14 @@ struct CustomTabItem<Content: View>: View {
             }
             .padding(5)
             .frame(maxWidth: 70, maxHeight: 70)
+            .onTapGesture {
+                withAnimation {
+                    // 이 두개의 PageChangetype이 다를 때 페이지 이동
+                    if navigationSelection != isSelection {
+                        navigationSelection = isSelection
+                    }
+                }
+            }
         }
     }
 }
