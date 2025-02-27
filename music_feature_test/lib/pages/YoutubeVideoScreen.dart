1b0507a6b38b2f7a3abb1f6b4a749b4759eb5734
김용해 <yoho1019@gmail.com>
2025-02-27 09:37:14 +0900

    youtube_player_flutter 테스트 코드 생성

--- music_feature_test/lib/pages/YoutubeVideoScreen.dart
+++ music_feature_test/lib/pages/YoutubeVideoScreen.dart
@@ -1,10 +1,38 @@
 import 'package:flutter/material.dart';
+import 'package:youtube_player_flutter/youtube_player_flutter.dart';
 
-class YoutubeViewScreen extends StatelessWidget {
+class YoutubeViewScreen extends StatefulWidget {
   const YoutubeViewScreen({super.key});
 
   @override
+  State<YoutubeViewScreen> createState() => _YoutubeViewScreenState();
+}
+
+class _YoutubeViewScreenState extends State<YoutubeViewScreen> {
+  final YoutubePlayerController _controller = YoutubePlayerController(
+    initialVideoId: 'nvqj4yPSt0g',
+    flags: YoutubePlayerFlags(autoPlay: true, mute: false),
+  );
+
+  @override
   Widget build(BuildContext context) {
-    return Scaffold(body: Center(child: Text("YoutubeVideoTest Page")));
+    return Scaffold(
+      body: SafeArea(
+        child: YoutubePlayer(
+          controller: _controller,
+          showVideoProgressIndicator: true,
+          progressColors: ProgressBarColors(
+            playedColor: Colors.red,
+            handleColor: Colors.redAccent,
+          ),
+          progressIndicatorColor: Colors.blue,
+          onReady: () {
+            _controller.addListener(listener);
+          },
+        ),
+      ),
+    );
   }
+
+  void listener() {}
 }
